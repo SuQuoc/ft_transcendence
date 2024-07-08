7d44e73724d3bfd3b05d4cfa98722ca5beb2c262
SuQuoc <tran_quoc_su@hotmail.com>
2024-07-08 19:52:58 +0200

    easy: black formatter skips quote standard

--- src/user_management/api/admin.py
+++ src/user_management/api/admin.py
@@ -1,21 +1,24 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser, MatchRecord
+
+from .models import CustomUser
+from .models import MatchRecord
 
 
 class CustomUserAdmin(UserAdmin):
     fieldsets = (
-    	*UserAdmin.fieldsets,
-		(
-			'Additional Info',
-			{
-				"fields": (
-					'online_status',
-					'friends',
-				),
-			}
-		)
-	)
+        *UserAdmin.fieldsets,
+        (
+            'Additional Info',
+            {
+                "fields": (
+                    'online_status',
+                    'friends',
+                ),
+            },
+        ),
+    )
+
 
 # Register your models here.
 admin.site.register(CustomUser, CustomUserAdmin)
