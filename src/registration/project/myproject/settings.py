cc99426b3298af6ef899548c4e52fc3f71c64725
aguilmea <aguilmea@debian12.myguest.virtualbox.org>
2024-07-13 21:13:37 +0200

    feat: django/back registration container is now connected to the postgres database container
    registration.yml: db_container is using a .env variable as exposed port, django/back container uses .env variable as well and the user and passord have been corrected to the access user credentials
    requirements.txt: added python-dotenv for getting env variables and psycopg2-binary for connection to the database container
    settings.py: use of postgres database and dotenv to get the environment variables

--- src/registration/project/myproject/settings.py
+++ src/registration/project/myproject/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv # [aguilmea] added manually to use for importing env
+import os # [aguilmea] added manually
+
+load_dotenv() # [aguilmea] added manually
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,12 +79,25 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# [aguilmea] database changed
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_ACCESS_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_ACCESS_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 
 # Password validation
