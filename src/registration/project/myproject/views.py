885b715d2c9be6872020e6a92e64e92b0483f9b4
aguilmea <aguilmea@debian12.myguest.virtualbox.org>
2024-07-13 19:09:35 +0200

    feat: django project setup
    Dockerfile: uses the latest bookworm version of python image and is written according to the image documentation
    requirements.txt include django and django restframework but not the dependancies that are installed automatically (version of the two is the latest one when attaching to the python image of the Dockerfile)
    test.rest: file to tests simple requests
    urls.py: include 3 urls for login signup and test
    views.py: return empty json response for now for all 3 types of request

---
+++ src/registration/project/myproject/views.py
@@ -0,0 +1,15 @@
+# [aguilmea] this file has been created manually
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['POST'])
+def login(request):
+    return Response({})
+
+@api_view(['POST'])
+def signup(request):
+    return Response({})
+
+@api_view(['GET'])
+def test_token(request):
+    return Response({})
