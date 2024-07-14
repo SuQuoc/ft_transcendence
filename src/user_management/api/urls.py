7d3820d4fa348f39dd9928e2d04bdfd367c6c8a9
SuQuoc <tran_quoc_su@hotmail.com>
2024-07-14 12:40:10 +0200

    fix: updated api name in url pattern

--- src/user_management/api/urls.py
+++ src/user_management/api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.UserList.as_view()),  # discard when frontend is ready
-    path("<int:pk>/", views.UserDetail.as_view()),  # discard when frontend is ready
+    path("", views.CustomUserList.as_view()),  # discard when frontend is ready
+    path("<int:pk>/", views.CustomUserDetail.as_view()),  # discard when frontend is ready
 ]
