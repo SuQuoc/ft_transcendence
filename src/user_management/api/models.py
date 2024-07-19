1ff2acfd8f81b53c85f807a4cd7a792f1afb55c3
Quocsu <anhsu.vie@gmail.com>
2024-07-19 14:11:51 +0200

    refactor: MatchRecord not part of user_management service

--- src/user_management/api/models.py
+++ src/user_management/api/models.py
@@ -33,13 +33,13 @@ class FriendRequest(models.Model):
 
 # all the records associated with the user: user.matchrecord_set.all()
 # with the related name i can easier filter for all the records where this user has won: user.wins.all()
-class MatchRecord(models.Model):
-    winner = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="wins")
-    loser = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="losses")
-    winner_score = models.IntegerField()
-    loser_score = models.IntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f"Score: {self.winner_score} - {self.loser_score}\nWinner: {self.winner.username}\nLoser: {self.loser.username}"
+# class MatchRecord(models.Model):
+#     winner = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="wins")
+#     loser = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="losses")
+#     winner_score = models.IntegerField()
+#     loser_score = models.IntegerField()
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+# 
+#     def __str__(self):
+#         return f"Score: {self.winner_score} - {self.loser_score}\nWinner: {self.winner.username}\nLoser: {self.loser.username}"
