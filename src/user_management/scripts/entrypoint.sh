39d88f1a37e3526bb805493a0450d49ab121f8d6
Quocsu <anhsu.vie@gmail.com>
2024-07-19 14:10:29 +0200

    refactor: creation of testusers

--- src/user_management/scripts/entrypoint.sh
+++ src/user_management/scripts/entrypoint.sh
@@ -6,11 +6,15 @@ set -e
 python manage.py makemigrations api
 python manage.py migrate
 
+# Creating django admin user
 cat << EOF | python manage.py shell
 from django.contrib.auth import get_user_model
 User = get_user_model()
 if not User.objects.filter(username='$DJ_SUDO_USERNAME_MANAGEMENT').exists():
     User.objects.create_superuser('$DJ_SUDO_USERNAME_MANAGEMENT', '$DJ_SUDO_EMAIL_MANAGEMENT', '$DJ_SUDO_PASSWORD_MANAGEMENT')
 EOF
+
+# Run custom command
+python manage.py create_testusers
 
 exec "$@"
