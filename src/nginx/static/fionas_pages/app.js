fc31a36c354c756a300c4862643adf40ab8aa5ef
AnnaFiona <annafiona.plank@gmail.com>
2024-07-24 16:46:39 +0200

    beauty: put the scripts in the header, watching a course and it says you should do that

---
+++ src/nginx/static/fionas_pages/app.js
@@ -0,0 +1,5 @@
+// its better to wait for the DOM to load before running any JS
+window.addEventListener("DOMContentLoaded", () => {
+    let nav = document.querySelector("nav");
+    console.log(nav);
+});
