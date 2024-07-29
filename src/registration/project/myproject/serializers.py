5dcbbae3970217a708ac6524f2787b5717dafd6b
Annie <annie.guilmeau@gmail.com>
2024-07-29 11:16:56 +0200

    beauty: changed name of project from myproject to project

--- src/registration/project/myproject/serializers.py
+++
@@ -1,8 +0,0 @@
-# [aguilmea] file was created manually
-from rest_framework import serializers
-from django.contrib.auth.models import User
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta(object):
-        model = User 
-        fields = ['id', 'username', 'password', 'email']
