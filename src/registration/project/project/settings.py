0780b478ffb438a141e0a330ce8024c74a68cb24
Annie <annie.guilmeau@gmail.com>
2024-08-02 17:31:17 +0200

    fix: problems with DEBUG variable and container names
    - we can now use the service name as container name for docker exec -it
    - the DEBUG variable was not set correctly

--- src/registration/project/project/settings.py
+++ src/registration/project/project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY') # [aguilmea] corrected to have it secret
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG') # [aguilmea] correct to have it in the .env
+DEBUG = os.environ.get('DEBUG') == True # [aguilmea] correct to have it in the .env
 
 ALLOWED_HOSTS = ['*'] # [aguilmea] corrected to have a value set up, mandatory in case of DEBUG ==false
 
