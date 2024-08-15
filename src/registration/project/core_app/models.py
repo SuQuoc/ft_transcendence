8823c8adc65646570b721a190136986d464626d8
Annie Guilmeau <aguilmea@c2r6p3.42vienna.com>
2024-08-15 12:29:17 +0200

    beauty: removed not necessary last login field from user model

--- src/registration/project/core_app/models.py
+++ src/registration/project/core_app/models.py
@@ -8,7 +8,6 @@ class CustomUser(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     username = models.EmailField(max_length=254, unique=True)
     password = models.CharField(max_length=128)
-    last_login = models.DateTimeField(blank=True, null=True)
     enabled2fa = models.BooleanField(default=False)
 
     REQUIRED_FIELDS = []
