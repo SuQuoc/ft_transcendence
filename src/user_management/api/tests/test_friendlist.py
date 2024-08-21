989d26a1c386b2716bf777c15e27b70feac9b3b7
Quocsu <qtran@student.42vienna.com>
2024-08-21 16:01:22 +0200

    test: changed to tests to work with cookies
    - wrote MyTestSetUp class for all tests to inherit to use provided utils
    - to run the test with successful results SIMPLE_JWT in settings must be changed to use normal secret which conflicts with production using key pair, no good solution found yet

--- src/user_management/api/tests/test_friendlist.py
+++ src/user_management/api/tests/test_friendlist.py
@@ -1,10 +1,9 @@
-
 import json
-from rest_framework.test import APITestCase
-from friends.models import FriendRequest
+
 from django.urls import reverse
-from utils_jwt import generate_token
-from .test_searching_user import create_friend_request_objs, create_user_objs
+from friends.models import FriendRequest
+from rest_framework.test import APITestCase
+from test_setup import MyTestSetUp
 
 ME = 'MeMario'
 I_FRIEND_U = 'i_friend_u'
@@ -16,17 +15,7 @@ U_DECLINE_ME = 'u_decline_me'
 STRANGER = 'Stranger'
 
 
-def contains_expected_user(response_json, expected_user):
-    print(f"Checking for expected user: {expected_user}")
-    for item in response_json:
-        print(f"Checking item: {item}")
-        if all(item.get(key) == value for key, value in expected_user.items()):
-            print("Match found!")
-            return True
-    print("No match found.")
-    return False
-
-class ViewingFriendPageTest(APITestCase):
+class ViewingFriendPageTest(MyTestSetUp):
     def setUp(self):
         self.test_users = [
             {'displayname': ME, 'online': True},
@@ -51,19 +40,16 @@ class ViewingFriendPageTest(APITestCase):
             (U_DECLINE_ME, ME, FriendRequest.DECLINED),
         ]
 
-        self.user_objs = create_user_objs(self.test_users)
-        self.friend_request_objs = create_friend_request_objs(self.user_objs, self.test_friend_requests)
+        self.user_objs = self.create_user_objs(self.test_users)
+        self.friend_request_objs = self.create_friend_request_objs(self.user_objs, self.test_friend_requests)
 
-        # generate token and set header
-        self.access_token = generate_token(self.user_objs[ME].user_id)
+        self.setup_jwt_with_cookie(self.user_objs[ME].user_id)
         self.headers = {
-            'HTTP_AUTHORIZATION': f'Bearer {self.access_token}',
+            # 'HTTP_AUTHORIZATION': f'Bearer {self.access_token}',
         }
 
         self.url = reverse("friends", kwargs={'user_id': self.user_objs[ME].user_id})
-    
-    
-    
+
     def test_friends(self):
         response = self.client.get(self.url, secure=True, **self.headers)
         response_json = response.json()
@@ -80,18 +66,27 @@ class ViewingFriendPageTest(APITestCase):
                 "relationship": "friend",
             },
             {
-                "displayname": I_PEND_U, # This person doesnt answer your request
+                "displayname": I_PEND_U,  # This person doesnt answer your request
                 "relationship": "requested",
             },
             {
                 "displayname": U_PEND_ME,
                 "relationship": "received",
-            },]
-        
+            },
+        ]
+
         # print(json.dumps(response_json, indent=4))
-        
+
         for expected_user in expected:
             self.assertTrue(contains_expected_user(response_json, expected_user))
 
 
-    
+def contains_expected_user(response_json, expected_user):
+    # print(f"Checking for expected user: {expected_user}")
+    for item in response_json:
+        # print(f"Checking item: {item}")
+        if all(item.get(key) == value for key, value in expected_user.items()):
+            # print("Match found!")
+            return True
+    # print("No match found.")
+    return False
