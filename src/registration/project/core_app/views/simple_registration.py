9f94edbb32a43967afef9f0b20dbba8672af9597
Annie Guilmeau <aguilmea@c1r2p4.42vienna.com>
2024-08-23 16:22:05 +0200

    feat: 2 authentication classes, one for requests with access tokens and one for requests with refresh token

--- src/registration/project/core_app/views/simple_registration.py
+++ src/registration/project/core_app/views/simple_registration.py
@@ -2,10 +2,9 @@ from datetime import datetime, timezone
 
 from django.conf import settings
 from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.decorators import permission_classes
-from rest_framework.permissions import AllowAny
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from ..authenticate import AccessTokenAuthentication, RefreshTokenAuthentication
 from rest_framework.response import Response
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.serializers import TokenRefreshSerializer
@@ -15,17 +14,55 @@ from ..models import CustomUser
 from ..serializers import LoginSerializer
 from ..serializers import UserSerializer
 
-def generate_response(status_code, token_s):
+import os
+
+def generate_response_with_valid_JWT(status_code, token_s):
     response = Response(status=status_code)
     if not token_s.is_valid():
         return Response(status=status.HTTP_400_BAD_REQUEST)
-    response.data = token_s.validated_data
     access_token = token_s.validated_data['access']
     access_token_expiration = datetime.now(timezone.utc) + settings.SIMPLE_JWT['ACCESS_TOKEN_LIFETIME']
-    response.set_cookie(key='access', value=access_token, expires=access_token_expiration, httponly=True)
+    response.set_cookie(
+        key='access',
+        value=access_token,
+        expires=access_token_expiration,
+        domain=os.environ.get('DOMAIN'),
+        httponly=True,
+        secure=True)
     refresh_token = token_s.validated_data['refresh']
     refresh_token_expiration = datetime.now(timezone.utc) + settings.SIMPLE_JWT['REFRESH_TOKEN_LIFETIME']
-    response.set_cookie(key='refresh', value=refresh_token, expires=refresh_token_expiration, httponly=True)
+    response.set_cookie(
+        key='refresh',
+        value=refresh_token,
+        expires=refresh_token_expiration,
+        domain=os.environ.get('DOMAIN'),
+        httponly=True,
+        secure=True)
+    return response
+
+
+def generate_response_with_invalid_JWT(status_code, token_s):
+    response = Response(status=status_code)
+    if not token_s.is_valid():
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    access_token = token_s.validated_data['access']
+    access_token_expiration = datetime.now(timezone.utc)
+    response.set_cookie(
+        key='access',
+        value=access_token,
+        expires=access_token_expiration,
+        domain=os.environ.get('DOMAIN'),
+        httponly=True,
+        secure=True)
+    refresh_token = token_s.validated_data['refresh']
+    refresh_token_expiration = datetime.now(timezone.utc)
+    response.set_cookie(
+        key='refresh',
+        value=refresh_token,
+        expires=refresh_token_expiration,
+        domain=os.environ.get('DOMAIN'),
+        httponly=True,
+        secure=True)
     return response
 
 
@@ -38,12 +75,13 @@ def signup(request):
             return Response(status=status.HTTP_400_BAD_REQUEST)
         user_s.save()
         token_s = TokenObtainPairSerializer(data=request.data)
-        return generate_response(status.HTTP_201_CREATED, token_s)
+        return generate_response_with_valid_JWT(status.HTTP_201_CREATED, token_s)
     except Exception as e:
-        return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 @api_view(['POST'])
+@authentication_classes([AccessTokenAuthentication])
 @permission_classes([IsAuthenticated])
 def delete_user(request):
 
@@ -61,7 +99,7 @@ def delete_user(request):
         response = Response(status=status.HTTP_200_OK)
         return response
     except Exception as e:
-        return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 @api_view(['POST'])
@@ -75,21 +113,26 @@ def login(request):
         if user is None or not user.check_password(credentials_s.validated_data['password']):
             return Response(status=status.HTTP_401_UNAUTHORIZED)
         token_s = TokenObtainPairSerializer(data=request.data)
-        return generate_response(status.HTTP_200_OK, token_s)
+        return generate_response_with_valid_JWT(status.HTTP_200_OK, token_s)
 
     except Exception as e:
         return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @api_view(['POST'])
+@authentication_classes([AccessTokenAuthentication])
 @permission_classes([IsAuthenticated])
 def change_password(request):
     try:
         current_password = request.data.get('current_password')
         new_password = request.data.get('new_password')
-        refresh = request.COOKIES.get('refresh')
-        if not current_password or not new_password or not refresh:
+        refresh = request.COOKIES.get('refresh', None)
+        access = request.COOKIES.get(settings['AUTH_COOKIE'])
+        if not access:
+            return Response({'message' : 'bla'}, status=status.HTTP_401_UNAUTHORIZED)
+        if not current_password or not new_password:
             return Response(status=status.HTTP_400_BAD_REQUEST)
-
+        if not refresh:
+            return Response({'access': access}, status=status.HTTP_401_UNAUTHORIZED)
         user = request.user
         if not user.check_password(current_password):
             return Response(status=status.HTTP_400_BAD_REQUEST)
@@ -101,10 +144,7 @@ def change_password(request):
         new_refresh = RefreshToken.for_user(user)
         new_access = new_refresh.access_token
         
-        response = Response({
-            'access': str(new_access),
-            'refresh': str(new_refresh)
-        }, status=status.HTTP_200_OK)
+        response = Response(status=status.HTTP_200_OK)
 
         access_token_expiration = datetime.now(timezone.utc) + settings.SIMPLE_JWT['ACCESS_TOKEN_LIFETIME']
         response.set_cookie(
@@ -125,38 +165,36 @@ def change_password(request):
         return response
 
     except Exception as e:
-        return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 @api_view(['GET'])
+@authentication_classes([AccessTokenAuthentication])
 @permission_classes([IsAuthenticated])
 def logout(request):
     try:
-        refresh_token = request.COOKIES.get('refresh')
-        if not refresh_token:
-            return Response({'error': 'Refresh token is required'}, status=status.HTTP_400_BAD_REQUEST)
-        token = RefreshToken(refresh_token)
-        token.blacklist()
-        return Response({'message': 'Logout successful'}, status=status.HTTP_205_RESET_CONTENT)
-
+        token_s = TokenObtainPairSerializer(data=request.data)
+        return generate_response_with_invalid_JWT(status.HTTP_200_OK, token_s)
     except Exception as e:
         return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 @api_view(['GET'])
-@permission_classes([AllowAny])
+@authentication_classes([RefreshTokenAuthentication])
+@permission_classes([IsAuthenticated])
 def refresh_token(request):
     try:
         refresh_token = request.COOKIES.get('refresh')
         if not refresh_token:
             return Response(status=status.HTTP_400_BAD_REQUEST)
         token_s = TokenRefreshSerializer(data={'refresh': refresh_token})      
-        return generate_response(status.HTTP_200_OK, token_s)
+        return generate_response_with_valid_JWT(status.HTTP_200_OK, token_s)
     except Exception as e:
         return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 @api_view(['GET'])
+@authentication_classes([AccessTokenAuthentication])
 @permission_classes([IsAuthenticated])
 def verify_token(request):
     return Response({'message': 'Token is valid'}, status=status.HTTP_200_OK)
--- src/registration/project/project/settings.py
+++ src/registration/project/project/settings.py
@@ -15,7 +15,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',  # [aguilmea] added manually
-    'rest_framework.authtoken',  # [aguilmea] added manually
     'rest_framework_simplejwt.token_blacklist',  # [aguilmea] added manually
     'corsheaders',  # [aguilmea] added manually for cookies
     'core_app',
@@ -88,17 +87,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # [aguilmea] added manually:
-# https://medium.com/django-unleashed/securing-django-rest-apis-with-jwt-authentication-using-simple-jwt-a-step-by-step-guide-28efa84666fe#id_token=eyJhbGciOiJSUzI1NiIsImtpZCI6ImUyNmQ5MTdiMWZlOGRlMTMzODJhYTdjYzlhMWQ2ZTkzMjYyZjMzZTIiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiIyMTYyOTYwMzU4MzQtazFrNnFlMDYwczJ0cDJhMmphbTRsamRjbXMwMHN0dGcuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiIyMTYyOTYwMzU4MzQtazFrNnFlMDYwczJ0cDJhMmphbTRsamRjbXMwMHN0dGcuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMTQxNDE4MjQ2NjQ3ODU1MTU1NTQiLCJlbWFpbCI6ImFubmllLmd1aWxtZWF1QGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJuYmYiOjE3MjIyNDM0MjQsIm5hbWUiOiJBbm5pZSBHIiwicGljdHVyZSI6Imh0dHBzOi8vbGgzLmdvb2dsZXVzZXJjb250ZW50LmNvbS9hL0FDZzhvY0pueFVDQ0s5SThZTi1ieTh5UnVqSkFKWVByYjl6RVhVYmVyQk9xM1h1VzQ3Nm5GQT1zOTYtYyIsImdpdmVuX25hbWUiOiJBbm5pZSIsImZhbWlseV9uYW1lIjoiRyIsImlhdCI6MTcyMjI0MzcyNCwiZXhwIjoxNzIyMjQ3MzI0LCJqdGkiOiJlYWQ1YWMzMWUxZmIzMDdmZjllNTQ3MWJlZDM0NDU4YjhkMjFkOWVkIn0.vsdjq_04QtB_urpSVlsDBoGX01_kCdTGWnER5oh_rruUi9mRq6mMOAOyJFAhJ32WcRJ1ah32eeBlYCo_gm2DSvvAo2OEdclVXdQQy2voYK5-UGLC957akmCBGapJ2pG80GmManTg6F2KcmHFJpY_S-WtV2pQUuS_ccpsGO299IQO0HWqV5eerZBQvfwfd_5CB_TkGjhtK7nqRo6RpP3LcZFQXEARKOSklJQJyS9C54LOYOx1jtUKT2AC3txC5HrIbxRQSqDG7Dsmld6YN6oow_rT5U82AjGzM0olgdOx87H9noa2XZIG8fN0-NNRSdY2fWlLIP_eAUohm0GNOQEi_w
-# https://pypi.org/project/django-cors-headers/
-
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
-}
 
 with open('/run/secrets/private_key.pem', 'r') as f:
     PRIVATE_KEY = f.read()
@@ -114,12 +102,6 @@ SIMPLE_JWT = {
     'ALGORITHM': 'RS256',
     'SIGNING_KEY': PRIVATE_KEY,
     'VERIFYING_KEY': PUBLIC_KEY,
-    'AUTH_COOKIE': 'access',  # Cookie name. Enables cookies if value is set.
-    'AUTH_COOKIE_DOMAIN': None,  # A string like "example.com", or None for standard domain cookie.
-    'AUTH_COOKIE_SECURE': False,  # Whether the auth cookies should be secure (https:// only).
-    'AUTH_COOKIE_HTTP_ONLY': True,  # Http only cookie flag.It's not fetch by javascript.
-    'AUTH_COOKIE_PATH': '/',  # The path of the auth cookie.
-    'AUTH_COOKIE_SAMESITE': 'Lax',  # Whether to set the flag restricting cookie leaks on cross-site requests. This can be 'Lax', 'Strict', or None to disable the flag.
 }
 
 CORS_ALLOWED_ORIGINS = [
@@ -129,6 +111,9 @@ CORS_ALLOWED_ORIGINS = [
 CORS_ALLOW_CREDENTIALS = True
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': ('core_app.authenticate.CustomAuthentication',),
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'core_app.authenticate.AccessTokenAuthentication',
+        'core_app.authenticate.RefreshTokenAuthentication',
+    ],
 }
 # [aguilmea] end of added manually
