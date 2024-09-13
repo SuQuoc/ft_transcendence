87ee1dbe45b2ffc0a70d2cf912f672992244d2c9
Annie Guilmeau <aguilmea@c2r6p3.42vienna.com>
2024-09-13 13:02:11 +0200

    feat: delete_user endpoint is now sending a delete request to usermanagement

--- src/registration/project/core_app/views/utils.py
+++ src/registration/project/core_app/views/utils.py
@@ -5,6 +5,7 @@ from django.conf import settings
 
 from django.core.mail import send_mail
 
+import requests
 import os
 
 def send_200_with_expired_cookies():
@@ -66,3 +67,15 @@ def send_reset_email(recipient, token):
         )
     except Exception as e:
         return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+def send_delete_request_to_um(request):
+    request_uri = 'http://usermanagement:8000/um/profile/'
+    headers = {
+        'Content-Type': 'application/json',
+        'Cookie': 'access=' + request.COOKIES.get('access')
+    }
+    response = requests.delete(request_uri, headers=headers)
+    if response.status_code != 204:
+        raise Exception('Error deleting user in UM')
+    return Response(status=status.HTTP_200_OK)
