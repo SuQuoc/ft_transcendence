89479cfc0ad8f9735aaf412a66b0790fb45f40d0
Quocsu <qtran@student.42vienna.com>
2024-09-13 18:03:09 +0200

    fix: search endpoint returning all users on wrong query key or empty string
    - if no user is found 200 OK with an empty body is returned
    - also changed that strangers get the relationship key with the value "stranger"
    - adapted api tests in django to the new relationship key
    - added .rest api test to manually test the search endpoint

--- src/user_management/api/urls.py
+++ src/user_management/api/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("user-creation/", views.CustomUserCreate.as_view(), name="user-creation"),
     path("profile/", views.CustomUserProfile.as_view(), name="profile"),
-    path("search/", views.SearchUserView.as_view(), name="search"),
+    path("search", views.SearchUserView.as_view(), name="search"),
 ]
