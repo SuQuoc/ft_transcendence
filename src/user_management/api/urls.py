5b5c91f94fbc6c1e8feaa9dd29e3870450298f65
Quocsu <qtran@student.42vienna.com>
2024-09-23 16:07:01 +0200

    fix: cant remove slash at the endpoint (yet) !!!

--- src/user_management/api/urls.py
+++ src/user_management/api/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("user-creation/", views.CustomUserCreate.as_view(), name="user-creation"),
-    path("profile", views.CustomUserProfile.as_view(), name="profile"),
+    path("profile/", views.CustomUserProfile.as_view(), name="profile"),
     path("search", views.SearchUserView.as_view(), name="search"),
 ]
