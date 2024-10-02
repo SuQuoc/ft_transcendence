0453ed6557513dab4ac8ab2e29584569e470fa0d
Annie Guilmeau <aguilmea@student.42vienna.com>
2024-10-02 13:04:01 +0200

    feat: tests playwright - added tests for basic_signup

---
+++ tests/playwright_tests/test_basic_signup.py
@@ -0,0 +1,85 @@
+from playwright.sync_api import Playwright, sync_playwright, expect
+from conftest import delete_user, set_display_name
+
+BASE_URL = "https://localhost:8000"
+USERMAIL = "transcendence42vienna+test1@gmail.com"
+USERPW = "password"
+USERDISPLAYNAME = "test1"
+OTP = "0000000000000000"
+
+class TestBasicSignup:
+
+    def test_signup_check_empty_fields(playwright: Playwright) -> None:
+       
+        with sync_playwright() as playwright:
+            
+            browser = playwright.chromium.launch(headless=False)
+            context = browser.new_context(ignore_https_errors=True)
+            page = context.new_page()
+        
+            try:
+                page.goto(BASE_URL)
+                page.click("#loginGoToSignup")
+
+            # if nothing is filled, the submit buttons should be disabled
+                expect(page.locator("#signupEmail")).to_be_empty()
+                expect(page.locator("#signupPassword1")).to_be_empty()
+                expect(page.locator("#signupPassword2")).to_be_empty()
+                expect(page.locator("#requestOTP")).to_be_disabled()
+                expect(page.locator("#signupSubmitButton")).to_be_disabled()
+
+            # if email and pw1 are filled, the submit buttons should be disabled
+                page.locator("#signupEmail").fill(USERMAIL)
+                page.locator("#signupPassword1").fill(USERPW)
+                page.locator("#signupPassword2").fill("")
+                expect(page.locator("#requestOTP")).to_be_disabled()
+                expect(page.locator("#signupSubmitButton")).to_be_disabled()
+
+            # if email and pw2 are filled, the submit buttons should be disaabled
+                page.locator("#signupEmail").fill(USERMAIL)
+                page.locator("#signupPassword1").fill("")
+                page.locator("#signupPassword2").fill(USERPW)
+                expect(page.locator("#requestOTP")).to_be_disabled()
+                expect(page.locator("#signupSubmitButton")).to_be_disabled()
+
+            # if pw1 and pw2 are filled, the submit buttons should be disaabled
+                page.locator("#signupEmail").fill("")
+                page.locator("#signupPassword1").fill(USERPW)
+                page.locator("#signupPassword2").fill(USERPW)
+                expect(page.locator("#requestOTP")).to_be_disabled()
+                expect(page.locator("#signupSubmitButton")).to_be_disabled()
+
+            # if all fields are filled, signup should be disabled, otp should be enabled
+                page.locator("#signupEmail").fill(USERMAIL)
+                page.locator("#signupPassword1").fill(USERPW)
+                page.locator("#signupPassword2").fill(USERPW)
+                expect(page.locator("#requestOTP")).to_be_enabled()
+                expect(page.locator("#signupSubmitButton")).to_be_disabled()
+
+            # sending the otp should disable otp and signup
+                page.locator("#requestOTP").click()
+                expect(page.locator("#requestOTP")).to_be_disabled()
+                expect(page.locator("#signupSubmitButton")).to_be_disabled()
+
+            # filling the otp should enable signup
+                page.locator("#otpCode").fill(OTP)
+                expect(page.locator("#requestOTP")).to_be_disabled()
+                expect(page.locator("#signupSubmitButton")).to_be_enabled()
+                page.locator("#signupSubmitButton").click()
+                
+            # finishing the signup and deleting the user
+                set_display_name(page, USERDISPLAYNAME)
+                delete_user(page, USERPW)
+            
+            except:
+                expect(page.locator("#FAIL")).to_be_visible() # causing an intended failure
+            
+            finally:
+                context.close()
+                browser.close()
+    
+
+    # ---------------------
+
+
+
