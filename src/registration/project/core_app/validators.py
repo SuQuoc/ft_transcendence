ba47411be1833a4e1264ddd6d2a1163b460df34f
Annie Guilmeau <aguilmea@student.42vienna.com>
2024-10-17 13:37:29 +0200

    feat: added validation rules

---
+++ src/registration/project/core_app/validators.py
@@ -0,0 +1,34 @@
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext, ngettext
+
+import re
+
+#copied form django.contrib.auth.password_validation MinimumLengthValidator
+class MyMaximumLengthValidator:
+    """
+    Validate that the password is of a maximum length.
+    """
+
+    def __init__(self, max_length=128):
+        self.max_length = max_length
+
+    def validate(self, password, user=None):
+        if len(password) > self.max_length:
+            raise ValidationError(
+                ngettext(
+                    "This password is too short. It must contain at least "
+                    "%(max_length)d character.",
+                    "This password is too short. It must contain at least "
+                    "%(max_length)d characters.",
+                    self.max_length,
+                ),
+                code="password_too_short",
+                params={"max_length": self.max_length},
+            )
+
+    def get_help_text(self):
+        return ngettext(
+            "Your password must contain at least %(max_length)d character.",
+            "Your password must contain at least %(max_length)d characters.",
+            self.max_length,
+        ) % {"max_length": self.max_length}
