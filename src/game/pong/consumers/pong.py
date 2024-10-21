7803eafa8feead402ad936b4cfbd65d4c6a4f1d1
AnnaFiona <annafiona.plank@gmail.com>
2024-10-21 15:07:14 +0200

    feature: made a very basic version of pong

--- src/game/pong/consumers/pong.py
+++ src/game/pong/consumers/pong.py
@@ -1,6 +1,133 @@
-import random
+import random # for old version of pong # maybe needed for ball in the new version (but i don't think so)!!
+
+
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+class Ball:
+    def __init__(self, pos_x, pos_y, vel_y, vel_x, size):
+        self.base_pos = Vector(pos_x, pos_y)
+        self.pos = Vector(pos_x, pos_y)
+        self.vel = Vector(vel_x, vel_y)
+        self.size = size
+    
+    def move(self):
+        self.pos.x += self.vel.x
+        self.pos.y += self.vel.y
+
+    def reset(self):
+        self.pos.x = self.base_pos.x # need to do it like this so it is a copy
+        self.pos.y = self.base_pos.y
+        self.vel = Vector(5, 5) # randomize direction ?!!!
+
+class Player:
+    def __init__ (self, id, pos_x, pos_y, width, height, speed):
+        self.id = id
+        self.pos = Vector(pos_x, pos_y)
+        self.width = width
+        self.height = height
+        self.speed = speed
+        self.score = 0
+        self.dir = "stop"
+
+    def move(self, max_height):
+        if self.dir == "up":
+            self.pos.y -= self.speed
+            if self.pos.y < 0:
+                self.pos.y = 0
+        elif self.dir == "down":
+            self.pos.y += self.speed
+            if self.pos.y + self.height > max_height:
+                self.pos.y = max_height - self.height
+
+
 
 class Pong:
+    def __init__(self, player_l, player_r, points_to_win):
+        self.points_to_win = points_to_win
+
+        self.canvas_width = 1000
+        self.canvas_height = 600
+        self.player_width = 10
+        self.player_height = 60
+
+        self.ball = Ball(self.canvas_width/2, self.canvas_height/2, 5, 5, 10)
+        self.player_l: Player = Player(id = player_l,
+                                        pos_x = self.player_width + 10,
+                                        pos_y = self.canvas_height/2 - self.player_height/2,
+                                        width = self.player_width,
+                                        height = self.player_height,
+                                        speed = 10)
+        self.player_r: Player  =  Player(id = player_r,
+                                        pos_x = self.canvas_width - self.player_width - 10,
+                                        pos_y = self.canvas_height/2 - self.player_height/2,
+                                        width = self.player_width,
+                                        height = self.player_height,
+                                        speed = 10)
+
+
+    def get_game_state(self):
+        return {
+            'player_l_y': self.player_l.pos.y,
+            'player_r_y': self.player_r.pos.y,
+            'ball_x': self.ball.pos.x,
+            'ball_y': self.ball.pos.y,
+            'score_l': self.player_l.score,
+            'score_r': self.player_r.score
+        }
+
+
+    def check_wall_collision(self, ball: Ball, canvas_width, canvas_height):
+        # reflect ball if it hits the top or bottom of canvas
+        if ball.pos.y <= 0 or ball.pos.y + ball.size >= canvas_height:
+            ball.vel.y *= -1
+        
+
+    def check_player_collision(self, ball: Ball, player_l: Player, player_r: Player):
+        if ball.pos.x <= player_l.pos.x:
+            if ball.pos.y + ball.size > player_l.pos.y and ball.pos.y < player_l.pos.y + player_l.height:
+                ball.vel.x *= -1 
+        if ball.pos.x + ball.size >= player_r.pos.x:
+            if ball.pos.y + ball.size > player_r.pos.y and ball.pos.y < player_r.pos.y + player_r.height:
+                ball.vel.x *= -1
+
+
+    def check_if_scored(self, ball: Ball, canvas_width):
+        if ball.pos.x < 0:
+            self.player_r.score += 1
+            ball.reset()
+        elif ball.pos.x > canvas_width:
+            self.player_l.score += 1
+            ball.reset()
+
+
+    def change_player_direction(self, player_id, direction):
+        if player_id == self.player_l.id:
+            self.player_l.dir = direction
+        elif player_id == self.player_r.id:
+            self.player_r.dir = direction
+
+
+    def update_game_state(self):
+        self.ball.move()
+        self.player_l.move(self.canvas_height)
+        self.player_r.move(self.canvas_height)
+
+        # check collision with players
+        self.check_player_collision(self.ball, self.player_l, self.player_r)
+        # check collision with walls or if players scored
+        self.check_wall_collision(self.ball, self.canvas_width, self.canvas_height)
+        # check if players scored
+        self.check_if_scored(self.ball, self.canvas_width)
+
+
+
+
+
+
+
+""" class Pong:
     def __init__(self, player_l, player_r):
         self.player_l = player_l
         self.player_r = player_r
@@ -51,11 +178,14 @@ class Pong:
         elif self.ball_x >= self.canvas_width - self.ball_size:
             self.score_l += 1
             self.reset_ball()
+
+
     def reset_ball(self):
         self.ball_x = self.canvas_width // 2
         self.ball_y = self.canvas_height // 2
         self.ball_dx = random.choice([-self.ball_speed, self.ball_speed])
         self.ball_dy = random.choice([-self.ball_speed, self.ball_speed])
+
 
     def get_game_state(self):
         return {
@@ -67,3 +197,4 @@ class Pong:
             'score_r': self.score_r
         }
     
+ """
--- src/game/pong/consumers/pong_game_consumer.py
+++ src/game/pong/consumers/pong_game_consumer.py
@@ -5,7 +5,122 @@ import asyncio
 from channels.generic.websocket import AsyncWebsocketConsumer
 from .pong import Pong
 
+
+
+
+
+
+
 class PongGameConsumer(AsyncWebsocketConsumer):
+    game_instance = None
+    #game_loop_running = False
+    
+    players = []
+    
+    async def connect(self):
+        #print(f"GameConsumer - connect")
+
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.game_group = f'game_{self.room_name}'
+        
+        await self.accept()
+        await self.channel_layer.group_add(
+                self.game_group,
+                self.channel_name
+        )
+
+        self.players.append(self.channel_name)
+        if len(self.players) == 2:
+            PongGameConsumer.game_instance = Pong(self.players[0], self.players[1], points_to_win=5)
+            await self.send_players_side()
+            self.players.remove(self.players[0])
+            self.players.remove(self.players[0])
+
+            #PongGameConsumer.game_loop_running = True
+            asyncio.create_task(self.game_loop())
+
+
+
+    async def disconnect(self, close_code):
+        #print(f"GameConsumer - disconnect")
+        if self.channel_name in self.players:
+            self.players.remove(self.channel_name)
+        await self.channel_layer.group_discard(
+            self.game_group,
+            self.channel_name
+        )
+        await super().disconnect(close_code)
+
+
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        type = text_data_json['type']
+
+        if type == 'up' or type == 'down' or type == 'stop':
+            print(f"received: {type}")
+            if PongGameConsumer.game_instance is None:
+                return
+            # changing the direction of the player that sent the message
+            PongGameConsumer.game_instance.change_player_direction(self.channel_name, type) # maybe raise error if player_id is not in players !!!
+        else:
+            return
+
+        
+    async def game_loop(self):
+        points_to_win = PongGameConsumer.game_instance.points_to_win
+        while True:
+            PongGameConsumer.game_instance.update_game_state()
+            game_state = PongGameConsumer.game_instance.get_game_state()
+
+            await self.channel_layer.group_send(
+                self.game_group,
+                {
+                    'type': 'state_update',
+                    'game_state': game_state
+                }
+            )
+            if game_state['score_l'] == points_to_win or game_state['score_r'] == points_to_win:
+                # send game end info to client
+                break
+
+            await asyncio.sleep(0.03)  # Update the game state every 50ms
+
+
+    async def state_update(self, event):
+        game_state = event['game_state']
+        await self.send(text_data=json.dumps({
+            'type': 'state_update',
+            'game_state': game_state
+        }))
+
+
+    async def send_players_side(self):
+        await self.channel_layer.send(
+            self.players[0],
+            {
+                'type': 'your_side',
+                'side': 'left'
+            }
+        )
+
+        await self.channel_layer.send(
+            self.players[1],
+            {
+                'type': 'your_side',
+                'side': 'right'
+            }
+        )
+
+    ### EVENTS - each Websocket sends message to frontend ###
+    async def your_side(self, event):
+        await self.send(text_data=json.dumps(event))
+
+
+
+
+
+
+""" class PongGameConsumer(AsyncWebsocketConsumer):
     game_instance = None
     #game_loop_running = False
     
@@ -47,12 +162,6 @@ class PongGameConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         type = text_data_json['type']
-        
-        
-    
-        #print(f"GameConsumer - receive:")
-        #print(json.dumps(text_data))
-        #print("\n")
 
         if type == 'up':
             direction = -1
@@ -62,11 +171,8 @@ class PongGameConsumer(AsyncWebsocketConsumer):
             return
 
         if PongGameConsumer.game_instance is None:
-            return    
-
-        ##print(self.channel_name)
-        ##print(PongGameConsumer.game_instance.player_l)
-        ##print(PongGameConsumer.game_instance.player_r)
+            return
+        
         if self.channel_name == PongGameConsumer.game_instance.player_l:
             PongGameConsumer.game_instance.player_l_y += PongGameConsumer.game_instance.paddle_speed * direction
         elif self.channel_name == PongGameConsumer.game_instance.player_r:
@@ -92,6 +198,7 @@ class PongGameConsumer(AsyncWebsocketConsumer):
                 break
 
             await asyncio.sleep(0.03)  # Update the game state every 50ms
+
 
     async def state_update(self, event):
         game_state = event['game_state']
@@ -121,3 +228,4 @@ class PongGameConsumer(AsyncWebsocketConsumer):
     ### EVENTS - each Websocket sends message to frontend ###
     async def your_side(self, event):
         await self.send(text_data=json.dumps(event))
+ """
