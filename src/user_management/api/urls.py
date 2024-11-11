80f595c45d08281547684936fbc3026dd7fa82ce
Annie <annie.guilmeau@gmail.com>
2024-11-11 20:23:00 +0100

    feat: new um endpoint get_displaynames

--- src/user_management/api/urls.py
+++ src/user_management/api/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     re_path(r"user-creation/?$", views.CustomUserCreate.as_view(), name="user-creation"),
     path("profile", views.CustomUserProfile.as_view(), name="profile"),
     path("search", views.SearchUserView.as_view(), name="search"),
+    path("get_displaynames", views.get_displaynames, name="get_displaynames"),
 ]
