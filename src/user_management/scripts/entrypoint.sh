a14194104a4f69936d40950fee37a0669ee9d7ed
Quocsu <qtran@student.42vienna.com>
2024-11-15 15:00:17 +0100

    fix: cant use any indentation when using heredoc

--- src/user_management/scripts/entrypoint.sh
+++ src/user_management/scripts/entrypoint.sh
@@ -12,10 +12,10 @@ python manage.py migrate
 if [ "$DEBUG" = "True" ]; then # TODO: ok like this?
     echo "Creating django admin user..."
     cat << EOF | python manage.py shell
-    from django.contrib.auth import get_user_model
-    User = get_user_model()
-    if not User.objects.filter(username='$DJ_SUDO_USERNAME').exists():
-        User.objects.create_superuser('$DJ_SUDO_USERNAME', '$DJ_SUDO_EMAIL', '$DJ_SUDO_PASSWORD')
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='$DJ_SUDO_USERNAME').exists():
+    User.objects.create_superuser('$DJ_SUDO_USERNAME', '$DJ_SUDO_EMAIL', '$DJ_SUDO_PASSWORD')
 EOF
     
     echo "Creating test users..."
