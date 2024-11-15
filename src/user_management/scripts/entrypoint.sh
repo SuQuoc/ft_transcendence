706cd573e3664a1993dbcc43802578ebe296e087
Quocsu <qtran@student.42vienna.com>
2024-11-15 14:43:08 +0100

    devops: removing bind mount + removed redis admin evn vars in game container
    - without bind-mount we need a way to have the script creating the redis.conf inside the container. So own Dockerfile needed

--- src/user_management/scripts/entrypoint.sh
+++ src/user_management/scripts/entrypoint.sh
@@ -9,18 +9,17 @@ python manage.py migrate
 #TODO: uncomment when checking for deployment
 #python manage.py collectstatic --noinput
 
-# Creating django admin user
-cat << EOF | python manage.py shell
-from django.contrib.auth import get_user_model
-User = get_user_model()
-if not User.objects.filter(username='$DJ_SUDO_USERNAME').exists():
-    User.objects.create_superuser('$DJ_SUDO_USERNAME', '$DJ_SUDO_EMAIL', '$DJ_SUDO_PASSWORD')
-EOF
-
 if [ "$DEBUG" = "True" ]; then # TODO: ok like this?
-    # Run custom command
+    echo "Creating django admin user..."
+    cat << EOF | python manage.py shell
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+    if not User.objects.filter(username='$DJ_SUDO_USERNAME').exists():
+        User.objects.create_superuser('$DJ_SUDO_USERNAME', '$DJ_SUDO_EMAIL', '$DJ_SUDO_PASSWORD')
+EOF
+    
     echo "Creating test users..."
-    python manage.py create_testusers 
+    python manage.py create_testusers # Run custom command
 fi
 
 exec "$@"
