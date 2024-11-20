b10f2f20befbf725499eb8e5646a46640f565f5d
Annie Guilmeau <aguilmea@student.42vienna.com>
2024-11-20 16:31:39 +0100

    fix: middleware with logging was throwing an error when uploading a profile image

--- src/user_management/api/middleware.py
+++ src/user_management/api/middleware.py
@@ -10,7 +10,16 @@ class LogRequestMiddleware:
         protocol = "HTTPS" if request.is_secure() else "HTTP"
         logging.warning(f"Received a {protocol} request to {request.get_full_path()}")
         logging.warning(f"Headers: {dict(request.headers)}")
-        logging.warning(f"Body: {request.body.decode('utf-8')}")
+
+        # Check Content-Type for binary data
+        if "application/json" in request.headers.get("Content-Type", "") or \
+           "text/" in request.headers.get("Content-Type", ""):
+            try:
+                logging.warning(f"Body: {request.body.decode('utf-8')}")
+            except UnicodeDecodeError:
+                logging.warning("Body could not be decoded as UTF-8.")
+        else:
+            logging.warning("Body is binary or not decodable.")
 
         response = self.get_response(request)
         return response
