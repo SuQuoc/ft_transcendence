1e469db04d8b8addfc5b25a276792e928ef21ec5
Quocsu <qtran@student.42vienna.com>
2025-01-13 16:51:50 +0100

    cleanup: friends app

--- src/user_management/friends/routing.py
+++ src/user_management/friends/routing.py
@@ -4,7 +4,6 @@ from django.urls import re_path, path
 from .online_status_consumer import OnlineStatusConsumer
 
 websocket_urlpatterns = [
-    # re_path(r"daphne/pong/(?P<room_name>\w+)/$", ChatConsumer.as_asgi()),
     re_path(r'um/online_status/?$', OnlineStatusConsumer.as_asgi()),
 ]
 
